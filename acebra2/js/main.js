b4020587d0221dd0a89ddaea0363c6b669ac2892
santanarscs <raphaelstn@gmail.com>
2018-10-20 15:41:22 -0300

    atualiza projeto

---
+++ acebra2/js/main.js
@@ -0,0 +1,61 @@
+$(function () {
+    $(".rslides").responsiveSlides({
+        auto: true,             // Boolean: Animate automatically, true or false
+        speed: 500,            // Integer: Speed of the transition, in milliseconds
+        timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+        pager: false,           // Boolean: Show pager, true or false
+        nav: false,             // Boolean: Show navigation, true or false
+        random: false,          // Boolean: Randomize the order of the slides, true or false
+        pause: false,           // Boolean: Pause on hover, true or false
+        pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+        prevText: "Previous",   // String: Text for the "previous" button
+        nextText: "Next",       // String: Text for the "next" button
+        maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+        navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+        manualControls: "",     // Selector: Declare custom pager navigation
+        namespace: "rslides",   // String: Change the default namespace used
+        before: function(){},   // Function: Before callback
+        after: function(){}     // Function: After callback
+    });
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+        weekends: false
+    })
+    // $(window).on('scroll', _.debounce(function () {
+    //     var $nav = $('.header'),
+    //         navHeight = $nav.outerHeight(),
+    //         windowTop = $(this).scrollTop();
+    //     if (windowTop > navHeight) {
+    //         console.log('nav pequena')
+    //         $nav.addClass('small');
+    //     } else {
+    //         console.log('nav grande')
+    //         $nav.removeClass('small');
+    //     }
+    // }, 200))
+
+    $("#menu").metisMenu();
+});
+
+function openPrice(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
